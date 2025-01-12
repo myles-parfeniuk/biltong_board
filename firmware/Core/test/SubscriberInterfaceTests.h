10fd2586f2b118345847b5d22877e34cba0e3a63
myles-parfeniuk <myles.parfenyuk@gmail.com>
2025-01-12 12:17:16 -0800

    deleted literally everything lol

--- firmware/Core/test/SubscriberInterfaceTests.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void SubscriberInterfaceTests_validate_checksum_test();
-void SubscriberInterfaceTests_notify_test();
-void SubscriberInterfaceTests_mute_test();
-void SubscriberInterfaceTests_mute_multi_cb_test();
-void SubscriberInterfaceTests_run_all();
--- firmware/Core/vendor/OPEEngine/OPEEngine_freeRTOS_hook.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include "semphr.h"
-#include "event_groups.h"
